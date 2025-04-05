555e59738d4b773a558e1042c6bba0efc034cd66
Jyoti Adala <jyotiadala24@gmail.com>
2025-04-05 14:27:17 -0400

    applied corrected GC skew function to rDNA locus in different window sizes in a loop

--- human/human_rdna_gc_skew.R
+++ human/human_rdna_gc_skew.R
@@ -8,11 +8,11 @@
 # for first analysis, I already defined a function to calculate GC skew  
 
 #load GC skew function
-source("/Users/jyotiadala/Library/CloudStorage/OneDrive-SUNYUpstateMedicalUniversity/project/bruce_lab/project/rDNA/rloop_and_rdna/human/one_rDNA_seq/script/gc_skew_function.R")
+source("/Users/jyotiadala/Library/CloudStorage/OneDrive-SUNYUpstateMedicalUniversity/project/bruce_lab/project/rDNA/rloop_and_rdna/human/one_rDNA_seq/script/human/gc_skew_function.R")
 rdna_human<- fread ("rdna_hg38_chr21_2018_dataset_details_v2.csv", sep = ",", header = TRUE)
 
 
-library(stringr)
+library(stringr) #needed for GC skew function
 library(tidyverse)
 library(data.table)
 
@@ -27,11 +27,12 @@ for ( i in 1:nrow(rdna_human)){
 
 
 
-selected_gc_skew_data <- attempt1 %>% select(GC_skew_value, norm_GC_skew) 
+selected_gc_skew_data <- attempt1 %>% select(GC_skew_value) 
 rdna_human<- cbind(rdna_human, selected_gc_skew_data)
 # wanted to add cumulative sum of total nucleotides for future 
-
+rdna_human<- rdna_human %>% mutate(norm_GC_skew = GC_skew_value/sum(GC_skew_value))
 rdna_human$x_axis <- cumsum(rdna_human$Total_nucleotides)
+
 fwrite(rdna_human, "rdna_hg38_chr21_2018_dataset_details_v3.csv")
 
 full_length_gc_skew<- ggplot(rdna_human, aes(x = x_axis, y = norm_GC_skew)) +
@@ -77,97 +78,147 @@ ggsave("KY962518_full_length_excld_IGS_normalised_gc_skew.tiff",
        plot = full_length_gc_skew_excld_igs, width=18, height = 10, dpi = 150)
 
 
-# The resulst are not that informative, so decided to to GC skew in 100 bp sliding window
+# Above result showed that there is a GC skew rDNA region. the maximum is seen in promoter region, followed by 28s region than 5'ETS. 
+# lowest is seen in 18S.
 
-library(Biostrings)
+# I made a loop that will calculate GC skew and GC content in 40, 70, 100 overlapping and non-overlapping window size for rdna locus (as a whole not compartmentalized) 
+#starting from 5'ETS and ending at 3'ETS).
+
+
+library(Biostrings) #needed to read fasta file
+#load GC skew and GC content function
+#calculate GC skew
+source("/Users/jyotiadala/Library/CloudStorage/OneDrive-SUNYUpstateMedicalUniversity/project/bruce_lab/project/rDNA/rloop_and_rdna/human/one_rDNA_seq/script/human/gc_skew_function.R")
+
+#calculate GC content
+source("/Users/jyotiadala/Library/CloudStorage/OneDrive-SUNYUpstateMedicalUniversity/project/bruce_lab/project/rDNA/rloop_and_rdna/human/one_rDNA_seq/script/human/GC_content_function.R")
+
 
 rdna_human_seq<- readDNAStringSet("KY962518_added_3500nt_IGS_upstream_nontemplate.fasta")
-seq<- as.character(rdna_human_seq[[1]])
+seq<- as.character(rdna_human_seq[[1]]) 
+seq2<- str_sub(seq, 3501, 16832) #just decided to have 5'ETS to 3'ETS
+nchar(seq2) 
+#[1] 13332
 
-gc_skew_100bp_data <- gc_skew(seq, window_size = 100)
-fwrite(gc_skew_100bp_data, "KY962518_added_3500nt_IGS_upstream_nontemplate_gc_skew_data_100bp.csv", sep=",")
+bin_size=c(30, 70, 100)
+for ( i in bin_size){
+  
+  gc_skew_data<- gc_skew(seq2, window_size = i)
+  ovlp_skew<- gc_skew_data$sliding_window_results
+  fwrite(ovlp_skew, paste0("KY962518_5ETS_TO_3ETS_gc_skew_ovlp_data_", i,"bp.csv"))
+  
+  ovlp_graph<- ggplot(ovlp_skew, aes(x = start, y = GC_skew_value)) +
+    geom_line(color = "blue") +
+    geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+    labs(title = "GC Skew Across rDNA Sequence KY962518",
+         subtitle= paste0(i, " bp ovlp Window Size"),
+         x = "Position (bp)",
+         y = "GC Skew ") +
+    scale_x_continuous(breaks = c(0, 3657,5526, 6596, 6753, 7920, 12971, 13333), 
+                       labels =c(0,"5'ETS", "18S","ITS1", "5.8S", "ITS2", "28S", "3'ETS"))+
+    theme(plot.title = element_text(hjust = 0.5, face = "bold"),
+          plot.subtitle = element_text(hjust = 0.5),
+          text = element_text(size = 30),
+          axis.line = element_line(color = "black"),
+          panel.grid = element_blank(),
+          panel.background = element_rect(fill = "white", color = "black"),
+          axis.title.y = element_text(angle = 90, vjust = 0.5, hjust = 0.5, size = 25), # Center Y-axis title
+          axis.ticks.y = element_line(color = "black"),
+          axis.text.x = element_text(angle = 45, hjust = 1, size=20))
+  
+  ggsave(paste0("KY962518_5ETS_TO_3ETS_gc_skew_ovlp_", i, "bp.tiff"), 
+         plot = ovlp_graph, width=18, height = 10, dpi = 150)
+  
+  
+  non_ovlp_skew<- gc_skew_data$fixed_window_results
+  fwrite(non_ovlp_skew, paste0("KY962518_5ETS_TO_3ETS_gc_skew_non_ovlp_data_", i,"bp.csv"))
+  
+  non_ovlp_graph<- ggplot(non_ovlp_skew, aes(x = start, y = GC_skew_value)) +
+    geom_line(color = "blue") +
+    geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+    labs(title = "GC Skew Across rDNA Sequence KY962518",
+         subtitle= paste0(i, " bp non ovlp Window Size"),
+         x = "Position (bp)",
+         y = "GC Skew ") +
+    scale_x_continuous(breaks = c(0, 3657,5526, 6596, 6753, 7920, 12971, 13333), 
+                       labels =c(0,"5'ETS", "18S","ITS1", "5.8S", "ITS2", "28S", "3'ETS"))+
+    theme(plot.title = element_text(hjust = 0.5, face = "bold"),
+          plot.subtitle = element_text(hjust = 0.5),
+          text = element_text(size = 30),
+          axis.line = element_line(color = "black"),
+          panel.grid = element_blank(),
+          panel.background = element_rect(fill = "white", color = "black"),
+          axis.title.y = element_text(angle = 90, vjust = 0.5, hjust = 0.5, size = 25), # Center Y-axis title
+          axis.ticks.y = element_line(color = "black"),
+          axis.text.x = element_text(angle = 45, hjust = 1, size=20))
+  
+  ggsave(paste0("KY962518_5ETS_TO_3ETS_gc_skew_non_ovlp_", i, "bp.tiff"), 
+         plot = non_ovlp_graph, width=18, height = 10, dpi = 150)
+  
+  
+  
+  
+  gc_content_data<- gc_content(seq2, window_size = i)
+  ovlp_content<- gc_content_data$sliding_window_results
+  fwrite(ovlp_content, paste0("KY962518_5ETS_TO_3ETS_gc_content_ovlp_data_", i,"bp.csv"))
+  
+  
+  ovlp_content_graph<- ggplot(ovlp_content, aes(x = start, y = gc_content_value)) +
+    geom_line(color = "blue") +
+    geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+    labs(title = "GC Content Across rDNA Sequence KY962518",
+         subtitle= paste0(i, " bp ovlp Window Size"),
+         x = "Position (bp)",
+         y = "GC content") +
+    scale_x_continuous(breaks = c(0, 3657,5526, 6596, 6753, 7920, 12971, 13333), 
+                       labels =c(0,"5'ETS", "18S","ITS1", "5.8S", "ITS2", "28S", "3'ETS"))+
+    theme(plot.title = element_text(hjust = 0.5, face = "bold"),
+          plot.subtitle = element_text(hjust = 0.5),
+          text = element_text(size = 30),
+          axis.line = element_line(color = "black"),
+          panel.grid = element_blank(),
+          panel.background = element_rect(fill = "white", color = "black"),
+          axis.title.y = element_text(angle = 90, vjust = 0.5, hjust = 0.5, size = 25), # Center Y-axis title
+          axis.ticks.y = element_line(color = "black"),
+          axis.text.x = element_text(angle = 45, hjust = 1, size=20))
+  
+  ggsave(paste0("KY962518_5ETS_TO_3ETS_gc_content_ovlp_", i, "bp.tiff"), 
+         plot =  ovlp_content_graph, width=18, height = 10, dpi = 150)
+  
+  
+  non_ovlp_content<- gc_content_data$fixed_window_results
+  fwrite(non_ovlp_content, paste0("KY962518_5ETS_TO_3ETS_gc_content_non_ovlp_data_", i,"bp.csv"))
+  
+  
+  non_ovlp_content_graph<- ggplot(non_ovlp_content, aes(x = start, y = gc_content_value)) +
+    geom_line(color = "blue") +
+    geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+    labs(title = "GC Content Across rDNA Sequence KY962518",
+         subtitle= paste0(i, " bp non ovlp Window Size"),
+         x = "Position (bp)",
+         y = "GC content") +
+    scale_x_continuous(breaks = c(0, 3657,5526, 6596, 6753, 7920, 12971, 13333), 
+                       labels =c(0,"5'ETS", "18S","ITS1", "5.8S", "ITS2", "28S", "3'ETS"))+
+    theme(plot.title = element_text(hjust = 0.5, face = "bold"),
+          plot.subtitle = element_text(hjust = 0.5),
+          text = element_text(size = 30),
+          axis.line = element_line(color = "black"),
+          panel.grid = element_blank(),
+          panel.background = element_rect(fill = "white", color = "black"),
+          axis.title.y = element_text(angle = 90, vjust = 0.5, hjust = 0.5, size = 25), # Center Y-axis title
+          axis.ticks.y = element_line(color = "black"),
+          axis.text.x = element_text(angle = 45, hjust = 1, size=20))
+  
+  ggsave(paste0("KY962518_5ETS_TO_3ETS_gc_content_non_ovlp_", i, "bp.tiff"), 
+         plot = non_ovlp_content_graph, width=18, height = 10, dpi = 150)
+  
+}
 
-gc_skew_50bp_data <- gc_skew(seq, window_size = 50)
-fwrite(gc_skew_50bp_data, "KY962518_added_3500nt_IGS_upstream_nontemplate_gc_skew_data_50bp.csv", sep=",")
 
-gc_skew_10bp_data <- gc_skew(seq, window_size = 10)
-fwrite(gc_skew_10bp_data, "KY962518_added_3500nt_IGS_upstream_nontemplate_gc_skew_data_10bp.csv", sep=",")
-
-
-
-
-
-
-
-
-gc100<- ggplot(gc_skew_100bp_data, aes(x = start, y = GC_skew_value)) +
-  geom_line(color = "blue") +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
-  labs(title = "GC Skew Across rDNA Sequence KY962518",
-       subtitle= "100 bp Sliding Window",
-       x = "Position (bp)",
-       y = "GC Skew ") +
-  scale_x_continuous(breaks = seq(0, 50000, by = 5000))+
-  theme(plot.title = element_text(hjust = 0.5, face = "bold"),
-        plot.subtitle = element_text(hjust = 0.5),
-        text = element_text(size = 30),
-        axis.line = element_line(color = "black"),
-        panel.grid = element_blank(),
-        panel.background = element_rect(fill = "white", color = "black"),
-        axis.title.y = element_text(angle = 90, vjust = 0.5, hjust = 0.5, size = 25), # Center Y-axis title
-        axis.ticks.y = element_line(color = "black"),
-        axis.text.x = element_text(angle = 45, hjust = 1, size=20))
-
-ggsave("KY962518_added_3500nt_IGS_upstream_gc_skew_100bp.tiff", 
-       plot = gc100, width=18, height = 10, dpi = 150)
-
-gc50<- ggplot(gc_skew_50bp_data, aes(x = start, y = GC_skew_value)) +
-  geom_line(color = "blue") +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
-  labs(title = "GC Skew Across rDNA Sequence KY962518",
-       subtitle= "50 bp Sliding Window",
-       x = "Position (bp)",
-       y = "GC Skew ") +
-  scale_x_continuous(breaks = seq(0, 50000, by = 5000))+
-  theme(plot.title = element_text(hjust = 0.5, face = "bold"),
-        plot.subtitle = element_text(hjust = 0.5),
-        text = element_text(size = 30),
-        axis.line = element_line(color = "black"),
-        panel.grid = element_blank(),
-        panel.background = element_rect(fill = "white", color = "black"),
-        axis.title.y = element_text(angle = 90, vjust = 0.5, hjust = 0.5, size = 25), # Center Y-axis title
-        axis.ticks.y = element_line(color = "black"),
-        axis.text.x = element_text(angle = 45, hjust = 1, size=20))
-
-ggsave("KY962518_added_3500nt_IGS_upstream_gc_skew_50bp.tiff", 
-       plot = gc50, width=18, height = 10, dpi = 150)
-
-
-gc10<- ggplot(gc_skew_10bp_data, aes(x = start, y = GC_skew_value)) +
-  geom_line(color = "blue") +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
-  labs(title = "GC Skew Across rDNA Sequence KY962518",
-       subtitle= "10 bp Sliding Window",
-       x = "Position (bp)",
-       y = "GC Skew ") +
-  scale_x_continuous(breaks = seq(0, 50000, by = 5000))+
-  theme(plot.title = element_text(hjust = 0.5, face = "bold"),
-        plot.subtitle = element_text(hjust = 0.5),
-        text = element_text(size = 30),
-        axis.line = element_line(color = "black"),
-        panel.grid = element_blank(),
-        panel.background = element_rect(fill = "white", color = "black"),
-        axis.title.y = element_text(angle = 90, vjust = 0.5, hjust = 0.5, size = 25), # Center Y-axis title
-        axis.ticks.y = element_line(color = "black"),
-        axis.text.x = element_text(angle = 45, hjust = 1, size=20))
-
-ggsave("KY962518_added_3500nt_IGS_upstream_gc_skew_10bp.tiff", 
-       plot = gc10, width=18, height = 10, dpi = 150)
-
-
+#if you want to visualize 
 library(karyoploteR)
 
-custom_genome <- toGRanges(data.frame(chr="rDNA_locus", start=1, end=48338))
+custom_genome <- toGRanges(data.frame(chr="rDNA_locus", start=1, end=48338)) #entire region rDNA including IGS
 kp <- plotKaryotype(genome=custom_genome, plot.type = 2)
 
 kpRect(kp, chr = 'rDNA_locus', x0 = 1, x1 =1298 , y0 = 0, y1 = 1, col = "#DE9A22", data.panel = "ideogram", borders= NA) #marks last 1298bp from IGS representing previous rdna 
